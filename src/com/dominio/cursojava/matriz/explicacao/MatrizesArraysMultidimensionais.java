e34eff6879e5262cf6e104e6b6d0cef2c19130c8
Mariana Chaves <79344040+marichaves@users.noreply.github.com>
2023-09-18 19:16:45 -0300

    base

--- src/com/dominio/cursojava/matriz/explicacao/MatrizesArraysMultidimensionais.java
+++ src/com/dominio/cursojava/matriz/explicacao/MatrizesArraysMultidimensionais.java
@@ -3,8 +3,21 @@ package com.dominio.cursojava.matriz.explicacao;
 public class MatrizesArraysMultidimensionais {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		double[][] notasAlunos = new double [30][4]; // Linhas x colunas
+		
+		//primeiro aluno
+		notasAlunos[0][0] = 10; //primeira nota do primeiro aluno
+		notasAlunos[0][1] = 8;
+		notasAlunos[0][2] = 8;
+		notasAlunos[0][3] = 10;
+		
+		//Segundo aluno
+		notasAlunos[1][0] = 8; //primeira nota do segundo aluno
+		notasAlunos[1][1] = 7;
+		notasAlunos[1][2] = 5;
+		notasAlunos[1][3] = 10;
+		
 	}
 
 }
