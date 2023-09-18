1e0f8d1c09588fc424d198f68fb49e470d4b2c0c
Mariana Chaves <79344040+marichaves@users.noreply.github.com>
2023-09-18 19:48:30 -0300

    new way

---
+++ src/com/dominio/cursojava/matriz/explicacao/ArrayEMatrizFormaDeDeclarar.java
@@ -0,0 +1,16 @@
+package com.dominio.cursojava.matriz.explicacao;
+
+public class ArrayEMatrizFormaDeDeclarar {
+
+	public static void main(String[] args) {
+		// Outr a maneira de declarar um array ou uma matriz:
+
+		// array:
+		double[] botasAluno1 = { 7, 8, 9, 10 };
+
+		// Matriz: (Aluno 1, aluno 2, aluno3)
+		double[][] notasAlunos = {{ 1, 3.6, 10, 10 }, { 1, 4, 10, 6 }, { 7, 8, 8, 10 }};
+
+	}
+
+}
